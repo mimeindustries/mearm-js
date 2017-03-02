cd1d3a3dfc5b82d9686e841c99d380a15428fcd8
Wayne Keenan <WayneKeenan@users.noreply.github.com>
2017-03-02 13:40:10 +0000

    added missing ref to PiJoysticks

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ module.exports = {
     MeArmPi:   require('./lib/MeArmPi'),
     DummyArm:  require('./lib/DummyArm'),
     MiroComms: require('./lib/MiroComms'),
-    PiJoysticks:    require('./lib/'),
+    PiJoysticks:    require('./lib/PiJoysticks'),
 }

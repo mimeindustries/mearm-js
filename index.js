6aa1858f28a05961ec971528a007eb41f3ae134f
waynekeenan <wayne.keenan@gmail.com>
2017-03-03 07:26:39 +0000

    move index and updated lib refs

--- index.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-    MeArmPi:   require('./lib/MeArmPi'),
-    DummyArm:  require('./lib/DummyArm'),
-    MiroComms: require('./lib/MiroComms'),
-    PiJoysticks:    require('./lib/PiJoysticks'),
-}
